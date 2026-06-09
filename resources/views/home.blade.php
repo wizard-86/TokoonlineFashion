6d06acd957e1f10484da502a7771307123a363f5
wizard-86 <informatikamalam1@gmail.com>
2026-06-09 20:36:43 +0700

    hapus home kucing di navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -47,7 +47,7 @@
 
                     <li class="nav-item">
                         <a class="nav-link" href="#home">
-                            Home {{$test}}
+                            Home
                         </a>
                     </li>
 
